d94e692bfbc036ed0a3afe2242eebb9f3051a6d9
Oli Salisbury <osalisbury@spindogs.com>
2017-10-30 07:35:51 +0000

    refactor currency display function

--- classes/Html.php
+++ classes/Html.php
@@ -31,21 +31,14 @@ class Html {
      */
     public static function currency($number, $hide_cents = false)
     {
-        if (strpos($number, '.') !== false ) {
+        $rtn = str_replace(',', '', $number);
+        $rtn = number_format($rtn, 2);
 
-            $fraction = strstr($number, '.', false);
-            $fraction = ltrim($fraction, '.');
-            $fraction = intval($fraction);
-
-            if ($fraction == 0 && $hide_cents) {
-                return number_format($number, 0);
-            } else {
-                return number_format($number, 2);
-            }
-
-        } else {
-            return number_format($number, 2);
+        if ($hide_cents) {
+            $rtn = str_replace('.00', '', $rtn);
         }
+
+        return $rtn;
     }
 
     /**
