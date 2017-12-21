ca3326524630d654556a0de9732b17d3324b86f1
Oli Salisbury <osalisbury@spindogs.com>
2017-12-21 11:17:34 +0000

    increase plugin version for last commit

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP-Platform V1
- * Version: 1.7.33
+ * Version: 1.7.34
  * Description: Platform to allow developers to build bespoke functionality in an MVC and OOP fashion
  */
 
