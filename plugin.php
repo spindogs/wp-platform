453d7e1c6474dd77b489c3988464a43ce9e4c892
Oli Salisbury <12654315+ol1s@users.noreply.github.com>
2019-09-03 13:53:36 +0100

    release v1.7.39

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP-Platform V1
- * Version: 1.7.37
+ * Version: 1.7.39
  * Description: Platform to allow developers to build bespoke functionality in an MVC and OOP fashion
  */
 
